a27947de454315a8b93b2cc42ec33081c6ada7b7
colugo <alastair.parker@gmail.com>
2018-01-18 15:55:30 +1100

    Basic markdown -> html

--- src/main/kotlin/Models.kt
+++ src/main/kotlin/Models.kt
@@ -1,3 +1,6 @@
+import com.vladsch.flexmark.html.HtmlRenderer
+import com.vladsch.flexmark.parser.Parser
+import com.vladsch.flexmark.util.options.MutableDataSet
 
 val definition = Page("Overview","",
         mutableListOf(
@@ -7,19 +10,34 @@ val definition = Page("Overview","",
 )
 
 
-open class TreeThing{}
+open abstract class TreeThing{
+    var name = ""
+    var subpages = mutableListOf<Page>()
+
+    override fun toString():String = name
+    abstract fun output():String
+
+    fun add(page:Page){
+        subpages.add(page)
+    }
+
+}
 
 class Service(val theName:String, var theSubpages: MutableList<Page> = mutableListOf<Page>()):TreeThing(){
 
-    var name = theName
-    var subpages = theSubpages
+    init {
+        name = theName
+        subpages = theSubpages
+    }
 
-    override fun toString():String = name
+    override fun output():String{
+        return toJson()
+    }
 
     fun toJson():String{
-        var output = "{\"name\":\"${name}\","
+        var output = "{\"name\":\"${name}\""
         output += ",\"subpages\":["
-        for(subpage in subpages) output += subpage
+        for(subpage in subpages) output += subpage.toJson()
         output += "]"
         return output
     }
@@ -27,18 +45,23 @@ class Service(val theName:String, var theSubpages: MutableList<Page> = mutableLi
 
 class Page(val theTitle: String, val theMarkdown: String, var theSubpages: MutableList<Page> = mutableListOf<Page>()):TreeThing(){
 
-    var title = theTitle
-    var markdown = theMarkdown
-    var subpages = theSubpages
+    init{
+        name = theTitle
+        subpages = theSubpages
+    }
 
-    override fun toString():String = title
+    var markdown = theMarkdown
+
+    override fun output():String{
+        return getMarkdown(markdown)
+    }
 
     fun toJson():String{
-        var output = "{\"title\":\"${title}\","
-        output += "\"markdown\":\"${markdown}\""
+        var output = "{\"title\":\"${name}\","
+        output += "\"markdown\":\"${sanatise()}\""
         if(subpages.isNotEmpty()) {
             output += ",\"subpages\":["
-            for(subpage in subpages) output += subpage
+            for(subpage in subpages) output += subpage.toJson()
             output += "]"
         }
 
@@ -46,8 +69,23 @@ class Page(val theTitle: String, val theMarkdown: String, var theSubpages: Mutab
         return output
     }
 
+    fun sanatise():String{
+        var output = markdown
+        output = output.replace("\n","\\n")
+        return output
+    }
 
-    fun add(page:Page){
-        subpages.add(page)
+
+    private fun getMarkdown(md:String):String{
+        val options = MutableDataSet()
+        val parser = Parser.builder(options).build()
+        val renderer = HtmlRenderer.builder(options).build()
+
+        // You can re-use parser and renderer instances
+        val document = parser.parse(md.trimMargin())
+        val html = renderer.render(document)  // "<p>This is <em>Sparta</em></p>\n"
+        return html
     }
 }
+
+
--- src/main/kotlin/Screen.kt
+++ src/main/kotlin/Screen.kt
@@ -4,47 +4,108 @@ import javafx.scene.Scene
 import javafx.scene.control.*
 import javafx.scene.layout.HBox
 import javafx.scene.layout.VBox
+import javafx.scene.web.WebView
 import javafx.stage.Stage
 
 class Screen : Application() {
 
+    val service = Service("service name")
+    val rootItem:TreeItem<TreeThing> = TreeItem(service)
+    val tree = TreeView(rootItem)
+    var globalCount = 0
+    var selectedItem:TreeThing? = null
+    var title = TextField("Title")
+    var markdown = TextArea("Markdown")
+    var browser = WebView()
+
+
     override fun start(primaryStage: Stage) {
         primaryStage.title = "Tree View Sample"
 
-
-        var service = Service("service name")
-
+        /*
         val rootItem:TreeItem<TreeThing> = TreeItem(service)
         rootItem.isExpanded = true
         for (i in 1..5) {
             val item = TreeItem(Page("Message" + i,"") as TreeThing)
             rootItem.children.add(item)
         }
-        val tree = TreeView(rootItem)
 
-        var hbox = HBox()
-        hbox.padding = Insets(15.0, 12.0, 15.0, 12.0)
-        hbox.setSpacing(10.0)   // Gap between nodes
-        hbox.children.add(tree)
+        val tree = TreeView(rootItem)
+        */
+
+
+        tree.selectionModel.select(rootItem)
+        tree.getSelectionModel()
+                .selectedItemProperty()
+                .addListener({ observable, oldValue, newValue -> updateViews(newValue.value as TreeThing) })
+
+        title.textProperty().addListener { observable, oldValue, newValue ->  updateSelectedThingName(newValue) }
+        markdown.textProperty().addListener { observable, oldValue, newValue ->  updateSelectedThingMarkdown(newValue) }
+
+
+        var mainHBox = HBox()
+        mainHBox.padding = Insets(15.0, 12.0, 15.0, 12.0)
+        mainHBox.setSpacing(10.0)   // Gap between nodes
+        var treeVBox = VBox()
+
+        treeVBox.children.add(tree)
+
+        var add = Button("Add new page")
+        add.setOnAction {
+            if(tree.selectionModel.selectedItems.size == 1) {
+                val selected = tree.selectionModel.selectedItems.first()
+                var newPage = Page("Page" + globalCount++, "")
+                selected.value.add(newPage)
+
+                val item = TreeItem(newPage)
+
+                selected.children.add(item as TreeItem<TreeThing>)
+                selected.isExpanded = true
+            }
+        }
+        treeVBox.children.add(add)
 
         var edit = VBox()
-        var textField = TextField("Title")
-        var textArea = TextArea("Markdown")
-        var add = Button("Add")
-        add.setOnAction {
-            val item = TreeItem(Page("Message" + 8,"") as TreeThing)
-            rootItem.children.last().children.add(item)
-            rootItem.children.last().isExpanded = true
-        }
-        edit.children.add(textField)
-        edit.children.add(textArea)
-        edit.children.add(add)
-        hbox.children.add(edit)
 
 
-        primaryStage.scene = Scene(hbox)
+        edit.children.add(title)
+        edit.children.add(markdown)
+
+        mainHBox.children.add(treeVBox)
+        mainHBox.children.add(edit)
+        mainHBox.children.add(browser)
+
+
+        primaryStage.scene = Scene(mainHBox)
         primaryStage.show()
 
+    }
+
+    fun updateSelectedThingName(newName:String){
+        if(selectedItem != null && selectedItem is TreeThing) {
+            selectedItem!!.name = newName
+            tree.refresh()
+        }
+    }
+
+    fun updateSelectedThingMarkdown(newMarkdown:String){
+        if(selectedItem != null && selectedItem is Page) {
+            var page = selectedItem as Page
+            page.markdown = newMarkdown
+            browser.engine.loadContent(selectedItem!!.output())
+        }
+    }
+
+    fun updateViews(thing:TreeThing){
+        selectedItem = thing
+        if(thing is Service){
+            title.text = thing.name
+            markdown.text = thing.output()
+        }
+        if(thing is Page){
+            title.text = thing.name
+            markdown.text = thing.markdown
+        }
     }
 
     companion object {
